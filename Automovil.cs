0873e9f83843d291c57f417d88a4cf50c28c67b3
Juan Cremona <104336519+juanupla@users.noreply.github.com>
2022-07-13 00:24:06 -0300

    Add files via upload
    Auto. Diseñar una clase que permita modelar el funcionamiento básico de un
    automóvil, tal como se indica a continuación:
     conducir: esta opción debe recibir la cantidad de kilómetros a recorrer e
    informar si pueden ser recorridos y en tal caso descontar el combustible
    utilizado del tanque (se conoce que, por especificaciones técnicas del
    vehículo, con un litro recorre 11 km). Caso contrario informar que no hay
    combustible suficiente para el recorrido indicado.
     cargar combustible: está opción debe reponer combustible en el tanque
    tantos litros como se reciban como argumento pero teniendo en cuenta que
    el tanque tiene una capacidad de 49 lts más una reserva de 5 lts., en el caso
    de que los litros informados superen la capacidad del tanque, devolver la
    cantidad de lts de combustible derramados.

---
+++ Automovil.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomovilApp
+{
+    class Automovil
+    {
+        //atributos:
+        private int odometro; //cantidad de km recorridos
+        private int ltsActuales;
+        private int capacidad;
+        private int reserva;
+        private int consumo;
+
+        public Automovil() //Constructores
+        {
+            odometro = 0;
+            ltsActuales = 0;
+            capacidad = 49;
+            reserva = 5;
+            consumo = 11;
+        }
+        public Automovil(int km, int ltsActuales):this()
+        {
+            odometro = km;
+            this.ltsActuales = ltsActuales;
+
+        }
+        //propiedades:
+        public int Odometro
+        {
+            get { return odometro; }
+            set { odometro = value; }
+        }
+        public int LtsActuales
+        { 
+            get { return ltsActuales; }
+            set { ltsActuales = value; }
+        }
+        public int Capacidad
+        { get { return capacidad; } }
+
+        public int Reserva
+        { get { return reserva; } }
+
+        public int Consumo
+        { get { return consumo;  } }
+        //Metodos de control:
+        public bool Conducir(int km)
+        {
+            bool aux = false;
+
+            int ltsnecesarios = km / consumo;
+            if (ltsActuales >= ltsnecesarios)
+            {
+                aux = true;
+                odometro += km;
+                ltsActuales -= ltsnecesarios;
+
+            }
+            return aux;
+        }
+        public int CargarCombustible(int lts)
+        {
+            int aux = 0;
+            int capacidadTotal = capacidad + reserva;
+            int ltsAux = ltsActuales + lts;
+            if(ltsActuales + lts >= capacidadTotal)
+            {
+                aux = capacidadTotal - ltsAux;
+                ltsActuales = capacidadTotal;
+
+            }
+            else
+            {
+                ltsActuales += lts;
+            }
+            return aux;
+        }
+        override //se puede usar el override que reescribr o omitirlo y al llamar auto2 xej usar el .ToString
+        public string ToString()
+        {
+            return "km: " + odometro + " |Nivel de combustible: " + ltsActuales;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿namespace AutomovilApp
+{
+    //Program es mi archivo ejecutable
+    internal class Program
+    {
+        static void Main(string[] arg) //Mine() es el punto de entrada a mi aplicacion
+        {
+            //creear nuestros objetos:
+            /*
+            Automovil auto = new Automovil(); //Puede declarar e inicializar una variable o puede ser Automovil auto; (y abajo) auto = new Automovil. Este metodo permite declarar mas variables de la misma clase
+            Automovil auto2 = new Automovil(1000, 20);
+
+            Console.WriteLine("Nivel de combustible actual:" + auto.LtsActuales);
+            Console.WriteLine("Auto 2: " + auto2);
+
+            /* Tambien se pueden pedir los datos de kmm y combustible x consola(usuario) */
+
+            //0-Pedir por teclado los datos de combustible y km actuales de un automovil:
+
+            
+
+            int kilometros, litros, litrosCarga, kmRecorrer;
+
+            Automovil oAutomovil; //oAutomovil = null;
+
+
+
+            Console.WriteLine("Ingrese cantidad de km del vehículo: ");
+
+            kilometros = int.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("Ingrese cantidad de litros de combustible del vehículo: ");
+
+            litros = Convert.ToInt32(Console.ReadLine());//Convert permite convertir una cadena a cualquier tipo
+
+            //de datos.
+
+
+
+            //1-Crear el automovil
+
+            oAutomovil = new Automovil(kilometros, litros);
+
+
+
+
+
+            //2-Cargar combustible al automovil:
+
+            Console.WriteLine("Ingrese cantidad litros a cargar: ");
+
+            litrosCarga = Convert.ToInt32(Console.ReadLine());
+
+            int derramados = oAutomovil.CargarCombustible(litrosCarga);
+
+
+
+            if (derramados == 0)
+
+                Console.WriteLine("Se cargaron " + litrosCarga + " lts. exitosamente!");
+
+            else
+
+                Console.WriteLine("Se llenó el tanque, se derramaron " + derramados + " lts!");
+
+
+
+            //3-Conducir una cierta cantidad de km:
+
+
+
+            Console.WriteLine("Ingrese cantidad km a recorrer: ");
+
+            kmRecorrer = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+            if (oAutomovil.Conducir(kmRecorrer) == true)
+
+                Console.WriteLine("Se recorrieron " + kmRecorrer + " kms.");
+
+            else
+
+                Console.WriteLine("No hay combustible suficiente para recorrer " + kmRecorrer + " kms.");
+        }
+    }
+}
